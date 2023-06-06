3e86937798dcdbb47cbb2311fd4e9e71cc6cb191
Shaheer Ahmed <msa034851@gmail.com>
2023-06-06 23:19:57 +0500

    Added list options to select multiple languages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 // import React from 'react';
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const App = () => {
   const [text, setText] = useState('')
   const [showAfterTranslation, setShowAfterTranslation] = useState('')
+  const [multipleLanguagesArray, setMultipleLanguagesArray] = useState([])
+  const [selectedLanguage, setSelectedLanguage] = useState('');
 
-  const fetchingData = async (textValue) => {
+  useEffect(() => {
+    const multipleLanguages = async () => {
+      const url = 'https://text-translator2.p.rapidapi.com/getLanguages';
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'ee7f409780mshf5b89eddc2790f1p1813c6jsn67caa6f07a7d',
+          'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+        }
+      };
+      try {
+        const response2 = await fetch(url, options);
+        const result = await response2.json();
+        setMultipleLanguagesArray(result.data.languages)
+      } catch (error) {
+        console.error(error);
+      }
+
+
+    }
+
+    setTimeout(multipleLanguages, 1000); // run it, run it
+  }, []);
+
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
+  const tranlatingData = async (textValue) => {
     const url = 'https://text-translator2.p.rapidapi.com/translate';
+
     const options = {
       method: 'POST',
       headers: {
@@ -16,24 +48,38 @@ const App = () => {
       },
       body: new URLSearchParams({
         source_language: 'en',
-        target_language: 'eu',
+        target_language: 'ur',
         text: textValue
       })
     };
 
     try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      setShowAfterTranslation(result.data.translatedText)
+      const response1 = await fetch(url, options);
+
+      const result1 = await response1.json();
+
+      setShowAfterTranslation(result1.data.translatedText)
     } catch (error) {
       console.error(error);
     }
   }
 
+
   return (
     <div>
+      <select value={selectedLanguage} onChange={handleLanguageChange}>
+        <option>Select a language</option>
+        {multipleLanguagesArray.map((language) => (
+          <option key={language.code} value={language.code}>
+            {language.name}
+          </option>
+        ))}
+      </select>
+
       <input value={text} name='inputingText' onChange={(e) => setText(e.target.value)} />
-      <button onClick={() => fetchingData(text)}> Submit </button>
+      <button onClick={() => tranlatingData(text)}> Submit </button>
+
+
 
       {showAfterTranslation}
     </div>
