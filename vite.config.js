b67bddf12a3ccdfa84e9a005eb5914749cc47ffe
Shaheer Ahmed <msa034851@gmail.com>
2024-05-19 14:56:00 +0100

    added docker file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Language-translator/',
   plugins: [react()],
+  server: {
+    host: true,
+    strictport: true,
+    // port: 8080
+  }
 })
